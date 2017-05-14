0adae94afde35bc0556252d19bab794717fa7160
Gale <george.gale@stfc.ac.uk>
2017-05-14 21:02:23 +0100

    Reorganise code and fix consistency with variables
    Generally you want your functions declared after the imports so that the main code is all in one easily readable block. Like paragraphing with prose, keep related blocks of code together, and separate code that controls different sections.
    
    When you declare variables, pick one style and be consistent with it. Either "var=10" or "var = 10".

--- Alarmclock.py
+++ Alarmclock.py
@@ -4,28 +4,33 @@ import time
 import os
 from tkinter import messagebox
 
-root = Tk()
-root.title("Alarm Clock")
 def SubmitButton():
-    AlarmTime= entry1.get()
+    AlarmTime = entry1.get()
     CurrentTime = time.strftime("%H:%M")
+    
     print("Alarm will ring at: {}".format(AlarmTime))
+    
     while AlarmTime != CurrentTime:
         CurrentTime = time.strftime("%H:%M")
         time.sleep(1)
+        
     if AlarmTime == CurrentTime:
         os.system("start Alarm.mp3")
+        
+root = Tk()
+root.title("Alarm Clock")
+
 frame1 = ttk.Frame(root)
 frame1.pack()
 frame1.config(height = 100, width = 100)
 
-label1= ttk.Label(frame1,text = "Enter the Alarm time:")
+label1 = ttk.Label(frame1,text = "Enter the Alarm time:")
 label1.pack()
 
 entry1 = ttk.Entry(frame1, width = 30)
 entry1.pack()
 
-button1= ttk.Button(frame1, text= "Submit", command=SubmitButton)
+button1 = ttk.Button(frame1, text= "Submit", command=SubmitButton)
 button1.pack()
 
 root.mainloop()
